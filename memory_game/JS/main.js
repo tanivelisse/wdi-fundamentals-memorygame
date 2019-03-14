63e94bff08d2417e5fb1a0ce21262716ca5f1ff5
Tania Medina <tanivelisse@gmail.com>
2019-03-14 08:40:45 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,22 +1,45 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank : 'queen',
+	suit : 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank : 'queen',
+	suit : 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank : 'king',
+	suit : 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},{
+	rank : 'king',
+	suit : 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+},
+];
+
 var cardsInPlay = [];
 
 //created game logic functions
 
 var checkForMatch = function()	{
 if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
+};
 }
-	}
 
 var flipcard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	if (cardsInPlay.length === 2) {
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
+ 	if (cardsInPlay.length ===2) {
 	checkForMatch();
-}
+};
 }
 
 
