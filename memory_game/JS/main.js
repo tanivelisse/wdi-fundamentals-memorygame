cb7c069b0f2b2834c82188ea944b1f2abb930426
Tania Medina <tanivelisse@gmail.com>
2019-03-14 05:56:43 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,17 +1,27 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if(cardsInPlay.length === 2) {
-	console.log("two cards selected");
-if (cardOne === cardTwo) {
-		alert("You found a match!")
-}else if (cardOne !== cardTwo) {
-	alert("Sorry try again.")
+
+//created game logic functions
+
+var checkForMatch = function()	{
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+	}
+
+var flipcard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
 }
 }
+
+
+
+flipcard(0);
+flipcard(1);
+
 
